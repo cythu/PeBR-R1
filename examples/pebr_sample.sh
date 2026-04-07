f5fd52ee4fca6be88924020e0d65dcc9bc74f546
cythu <1445081024@qq.com>
2026-04-07 22:24:19 +0800

    Update pebr_sample.sh

--- examples/pebr_sample.sh
+++ examples/pebr_sample.sh
@@ -8,7 +8,7 @@ MODEL_PATH=./pebr_r1_warm_up  # replace it with your local file path
 
 python3 -m verl.trainer.pebr_sample_main \
     config=examples/pebr_sample.yaml \
-    data.train_files=dataset/pebr_grpo_dataset.json \
+    data.train_files=dataset/grpo/pebr_grpo_dataset.json \
     data.val_files=hiyouga/math12k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.reward.reward_function=./examples/reward_function/pebr_sample.py:compute_score \
